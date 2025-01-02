d57b76ddc77a80d37aa8f4510dd2f77345c48ced
Francesco-Peluso <francesco.peluso001@studenti.uniparthenope.it>
2025-01-02 22:53:56 +0100

    Created the Blocks and DataBlock data structure
    Two new data structures created:
    -DataBlock, which represents the effective data block for file system's files
    -Blocks, a data structure that keeps track of all data block of in the file system
    
    The macro FILE_SYSTEM_SINGLE_BLOCK_SIZE defines the size of a single data block for storing data

---
+++ src/include/blocks/Blocks.hpp
@@ -0,0 +1,43 @@
+//
+// Created by frank on 12/30/24.
+//
+
+#ifndef BLOCKS_HPP
+#define BLOCKS_HPP
+
+#include <map>
+#include <vector>
+#include <fuse3/fuse_lowlevel.h>
+
+#include "DataBlock.hpp"
+
+using namespace std;
+
+class Blocks {
+private:
+	//Singleton implementation
+	static Blocks *instance;
+	Blocks();
+
+	map<fuse_ino_t, vector<DataBlock *> > FileSystemDataBlocks;
+
+public:
+	//Singleton implementation
+	static Blocks* getInstance();
+
+	~Blocks();
+
+	void createEmptyBlockListForInode(fuse_ino_t inode);
+	void setBlockListForInode(fuse_ino_t inode, vector<DataBlock *> &blockList);
+	bool blockListExistForInode(fuse_ino_t inode);
+	vector<DataBlock *> &getDataBlockListOfInode(fuse_ino_t inode);
+	DataBlock *addDataBlockToInode(fuse_ino_t inode);
+	void addDataBlockToInode(fuse_ino_t inode, DataBlock *dataBlock);
+	unsigned int getNumberOfUsedBlocksOfInode(fuse_ino_t inode);
+	unsigned int getTotalBlockBytesOfInode(fuse_ino_t inode);
+	bool hasNoBlocks(fuse_ino_t inode);
+};
+
+
+
+#endif //BLOCKS_HPP
---
+++ src/include/blocks/data_blocks_info.hpp
@@ -0,0 +1,10 @@
+//
+// Created by frank on 12/16/24.
+//
+
+#ifndef DATA_BLOCKS_INFO_HPP
+#define DATA_BLOCKS_INFO_HPP
+
+#define FILE_SYSTEM_SINGLE_BLOCK_SIZE 512
+
+#endif //DATA_BLOCKS_INFO_HPP
